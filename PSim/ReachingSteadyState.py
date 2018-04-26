06cbc4619dd93ab4cc4a5df83ecade7a63e267bf
naspitha <spitha@wisc.edu>
2018-04-26 10:47:48 -0500

    cleaning out
    Cleaned out the code for easier switching between type of figure

--- PSim/ReachingSteadyState.py
+++ PSim/ReachingSteadyState.py
@@ -2,15 +2,43 @@
 """
 Created on Mon Apr  2 20:22:39 2018
 
+
 @author: Natalia Spitha
 """
+
+########################### Imports / matplotlib params #######################
+
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-matplotlib.rcParams['font.size'] = 20 
+matplotlib.rcParams['font.size'] = 25
+matplotlib.rcParams['lines.linewidth'] = 3
 
+#######################Function-independent Definitions #######################
+fs = 1e-15
+ps = 1e-12
+ns = 1e-9
+us = 1e-6
+ms = 1e-3
+MHz = 1e6
+kHz = 1e3
+nm = 1e-9
+c = 2.9979e8
+hPlanck = 6.626e-34
+
+#time parameters & default values
+cwrate = 0
+pulsePower = 8e7
+reprate = 80*MHz
+spacing = 1.00/reprate
+N = 5000 #number of points in array corresponding to one cycle
+numcycles = 20
+duration = spacing*numcycles
+stepsize = spacing/N
+time = np.arange(0,duration,stepsize)
 
 ################################## Functions ##################################
+
 def Gauss(t, a, t0, sigma):
     return a * np.exp(-(t - t0)**2 / (2 * sigma**2))
 
@@ -94,17 +122,37 @@ def tlossEqn(f, h, FHloss=3.42e-12):
     tloss = f * h * FHloss
     return tloss
 
-############################### Definitions ###################################
-fs = 1e-15
-ps = 1e-12
-ns = 1e-9
-us = 1e-6
-ms = 1e-3
-MHz = 1e6
-kHz = 1e3
-nm = 1e-9
-c = 2.9979e8
-hPlanck = 6.626e-34
+
+def pulseP(power=pulsePower, wavelength=400*nm, reprate=80*MHz, fwhm=100*fs):
+    '''
+    Returns peak injected carrier density from a pulse given its power,
+    wavelength, rep rate, and FWHM
+    '''
+    photonEnergy = hPlanck*c/wavelength
+    pulseEnergy = power/reprate
+    pulseCarriers = pulseEnergy/photonEnergy
+    pulseSigma = fwhm/(2*np.sqrt(2*np.log(2)))
+    pulsePeak = pulseCarriers/(np.sqrt(2*np.pi)*pulseSigma)
+    return pulsePeak
+
+def pulse(t, pPeak=pulseP(pulsePower), reprate=80*MHz, fwhm=100*fs):
+    '''
+    produces the pulse (over a time range of 1/reprate) given its peak power 
+    and Gaussian parameters
+    '''
+    pulseSigma = fwhm/(2*np.sqrt(2*np.log(2)))
+    spacing = 1.00/reprate
+    return Gauss(t,pPeak,spacing/2,pulseSigma)
+
+#########################Function-dependent Definitions #######################
+
+#Production of pulse and pulse train
+pulsePeak = pulseP(power = pulsePower)
+print(pulsePeak)
+stime = np.linspace(0,spacing,N+1)[:-1]
+spulse = pulse(stime)
+stepsize = spacing/N
+pulseTrain = np.tile(spulse,numcycles)
 
 #Initial Parameters 
 g = 0
@@ -113,38 +161,13 @@ ho = 0
 ft = 0
 trap = 2.52e18 #cm^-3
 
-reprate = 80*MHz
-spacing = 1.00/reprate
-N = 5000 #number of points in array corresponding to one cycle
-numcycles = 20
-duration = spacing*numcycles
-stepsize = spacing/N
-cwrate = 100e28 #carriers created per cm^3 per s
-
-#Time and population arrays
-time = np.arange(0,duration,stepsize)
+#population arrays
 gs = np.zeros(len(time))
 es = np.zeros(len(time))
 hos = np.zeros(len(time))
 fts = np.zeros(len(time))
 gsigs = np.zeros(len(time))
 ehsigs = np.zeros(len(time))
-
-
-def pulse(t, power=0.008*1e10, wavelength=400*nm, reprate=80*MHz, fwhm=100*fs):
-    photonEnergy = hPlanck*c/wavelength
-    pulseEnergy = power/reprate
-    pulseCarriers = pulseEnergy/photonEnergy
-    spacing = 1.00/reprate
-    pulseSigma = fwhm/(2*np.sqrt(2*np.log(2)))
-    pulsePeak = pulseCarriers/(np.sqrt(2*np.pi)*pulseSigma)
-    return Gauss(t,pulsePeak,spacing/2,pulseSigma)
-    
-stime = np.linspace(0,spacing,N+1)[:-1]
-spulse = pulse(stime)
-stepsize = spacing/N
-
-pulseTrain = np.tile(spulse,numcycles)
 
 ############################ Numerical Integration ############################
 for i in np.arange(1,numcycles*N):
@@ -156,35 +179,43 @@ for i in np.arange(1,numcycles*N):
     ehsigs[i] = ehsignalEqn(es[i], hos[i])
 
 ############################ Plotting #########################################
-timens = time*1e9
 
 #an unnecessarily fancy way to generate labels
 if cwrate == 0:
-    cwlabel = 'no CW'
+    cwlabel = 'No CW Intensity'
 else:
     expform = '{0:0.4e}'.format(cwrate)
     split = expform.split('e')
     nvalue = float(split[0])
     pow10 = int(split[1])
-    cwlabel = '%0.2d'%nvalue + r'$\times 10^{' + str(pow10) + r'} \  cm^{-3} \ s^{-1}$'
+    cwlabel = '%0.2d'%nvalue + r'$\times 10^{' + str(pow10) + r'} \  cm^{-3} \ s^{-1}$ from CW'
 
-#plt.subplot(311)
-#plt.plot(timens, pulseTrain, label='pulse')
-#plt.ylabel('Pulse Intensity (Carriers/s)')
-#plt.subplot(211)
-#plt.plot(timens, gs, label = 'G')
-#plt.plot(timens, es, label = 'E')
-#plt.plot(timens, hos, label = 'H')
-#plt.plot(timens, fts, label = 'F')
-#plt.ylabel('Population')
-#plt.yscale('log')
-#plt.legend()
-#plt.subplot(212)
-plt.plot(timens, gsigs+ehsigs, label = cwlabel)
-plt.ylabel('PL Signal')
-#plt.xlim(1,117.5)
-#plt.yscale('log')
-#plt.ylim(2e27,)
-plt.legend()
-plt.xlabel('Time (ns)')
-plt.show()
+if pulsePower == 0:
+    pplabel = 'no pulse'
+else:
+    expformp = '{0:0.4e}'.format(pulsePeak)
+    splitp = expformp.split('e')
+    nvaluep = float(splitp[0])
+    pow10p = int(splitp[1])
+    pplabel = '%0.2d'%nvaluep + r'$\times 10^{' + str(pow10p) + r'} \  cm^{-3} \ s^{-1}$'
+    
+timens = time*1e9
+
+#if 0: #show evolution of carrier populations while reaching steady state
+    
+
+#Show signal evolution per cycle
+if 1: 
+    for n in np.array([1,2,3,5,10,15,20,25,30]): 
+        tstart = 6.1+12.5*(n-1)
+        print(tstart)
+        timens = time*1e9
+        timens-= tstart
+        plt.title("PL signal evolution")
+        plt.plot(timens, (gsigs+ehsigs), alpha = 0.6, label = n)
+    plt.xlim(-0.1,12.4)
+    plt.ylabel(r'Outcoming photons cm$^{-3}$ s$^{-1}$ ($k_{hcat}GH + k_{rec}EH$)')
+    plt.yscale('log')
+    plt.legend(loc=4,title='Cycle')
+    plt.xlabel('Time (ns)')
+    plt.show()
